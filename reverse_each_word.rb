ebe2368ac1f1ae9517031fe0881539abffb92f0c
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-23 18:30:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = [] 
   
-  array.each do |word|
+  array.each do |sentence|
   new_array << sentence.reverse
   end 
 new_array.join(" ")
